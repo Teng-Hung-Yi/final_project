8ae4874f6b81075009fe888f08c3dafe0be38799
Teng-Hung-Yi <55129034+Teng-Hung-Yi@users.noreply.github.com>
2020-05-27 18:46:08 +0800

    Update 10727205_final.R

--- 10727205_final.R
+++ 10727205_final.R
@@ -1,5 +1,22 @@
 rm(list=ls())
 
-setwd("c:")
+hardware_path <- "c:/"
+setwd(hardware_path)
 dir.create("metro_data")
-web <- "https://www.metro.taipei/cp.aspx?n=FF31501BEBDD0136"
+path <- "metro_data"
+
+#  http://web.metro.taipei/RidershipPerStation/202004_cht.ods
+
+download_data_file <- function( x ) {
+   
+  dest<- paste0( hardware_path,path,'/',x,"_cht.ods")
+  filename_Fhalf <- paste0("https://web.metro.taipei/RidershipPerStation/",x )
+  filename<- paste0( filename_Fhalf,"_cht.ods")
+  filename
+  
+  download.file(filename,destfile = dest, mode = "wb" )
+  
+}
+
+download_data_file(202004)
+
